4748c4c84ecf74950c3889d9c627f91c5fc33089
yosyo <yosyomiyori@gmail.com>
2023-10-08 19:35:31 +0200

    feat(weapon): added vertical flip

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 400
-@export var offset = 50
+@export var offset = 12
 @export var friction = 0.25
 @export var acceleration = 0.25
 var screen_size
@@ -9,10 +9,15 @@ var screen_size
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func _process(delta):
+func _process(_delta):
 	move_and_slide()
-	$Weapon.position = $Weapon.transform.x * offset;
-	$Weapon.look_at(get_global_mouse_position())
+	$Weapon/WeaponSprite.position = $Weapon/WeaponSprite.transform.x * offset;
+	$Weapon.rotation = lerp_angle($Weapon.rotation, $Weapon.rotation + $Weapon.get_angle_to(get_global_mouse_position()), 0.1)
+	if abs($Weapon.global_rotation_degrees) > 90:
+		$Weapon/WeaponSprite.flip_v = true
+	else:
+		$Weapon/WeaponSprite.flip_v = false
+	print($Weapon.global_rotation_degrees)
 
 func get_input():
 	var direction = Vector2.ZERO # The player's movement vector.
@@ -26,7 +31,7 @@ func get_input():
 		direction.y += 1
 	return direction
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = get_input();
 	if direction.length() > 0:
 		velocity = lerp(velocity, direction.normalized() * speed, acceleration)
