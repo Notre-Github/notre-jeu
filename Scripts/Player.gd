d608d46f2f827423e36788791ea6f41234d88b57
Abdel B. <abdel.briand.a@gmail.com>
2023-10-08 21:27:48 +0200

    Knife: Added a knife attack when left clicking + Shotgun as its own scene now.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,17 +9,9 @@ var screen_size
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func _process(_delta):
-	move_and_slide()
-	$Weapon/WeaponSprite.position = $Weapon/WeaponSprite.transform.x * offset;
-	$Weapon.rotation = lerp_angle($Weapon.rotation, $Weapon.rotation + $Weapon.get_angle_to(get_global_mouse_position()), 0.1)
-	if abs($Weapon.global_rotation_degrees) > 90:
-		$Weapon/WeaponSprite.flip_v = true
-	else:
-		$Weapon/WeaponSprite.flip_v = false
-	print($Weapon.global_rotation_degrees)
-
 func get_input():
+	if Input.is_action_pressed("confirm"):
+		get_node("Knife/AnimationPlayer").play("Knife_Slash")
 	var direction = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
@@ -37,3 +29,16 @@ func _physics_process(_delta):
 		velocity = lerp(velocity, direction.normalized() * speed, acceleration)
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, friction)
+
+func _process(_delta):
+	move_and_slide()
+	$Shotgun/ShotgunSprite.position = $Shotgun/ShotgunSprite.transform.x * offset;
+	$Shotgun.rotation = lerp_angle($Shotgun.rotation, $Shotgun.rotation + $Shotgun.get_angle_to(get_global_mouse_position()), 0.1)
+	if abs($Shotgun.global_rotation_degrees) > 90:
+		$Shotgun/ShotgunSprite.flip_v = true
+	else:
+		$Shotgun/ShotgunSprite.flip_v = false
+	
+	$Knife/KnifeSprite.position = $Knife/KnifeSprite.transform.x * offset;
+	$Knife.rotation = lerp_angle($Knife.rotation, $Knife.rotation + $Knife.get_angle_to(get_global_mouse_position()), 0.1)
+
